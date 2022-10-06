dfbbcdddf5c020dddc8b57d6372550e78e6d7ccf
ksh940925 <ksh9409255@naver.com>
2022-10-06 20:27:09 +0900

    feat: init setting

---
+++ src/main/java/com/team6/onandthefarmmemberservice/OnandthefarmMemberserviceApplication.java
@@ -0,0 +1,13 @@
+package com.team6.onandthefarmmemberservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnandthefarmMemberserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnandthefarmMemberserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team6/onandthefarmmemberservice/OnandthefarmMemberserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.onandthefarmmemberservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnandthefarmMemberserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
