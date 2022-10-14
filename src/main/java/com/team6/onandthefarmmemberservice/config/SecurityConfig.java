c3b3ed2b3965a6e70b8cf61a9eaa2d1763974d13
jikimomo <jikimomo@gmail.com>
2022-10-14 19:51:48 +0900

    feat : jwtAuthenticationFilter 수정

--- src/main/java/com/team6/onandthefarmmemberservice/config/SecurityConfig.java
+++ src/main/java/com/team6/onandthefarmmemberservice/config/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.team6.onandthefarmmemberservice.config;
 
 import com.team6.onandthefarmmemberservice.repository.UserRepository;
+import com.team6.onandthefarmmemberservice.security.jwt.JwtAuthenticationFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,25 +18,14 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @EnableWebSecurity // 시큐리티 활성화 -> 기본 스프링 필터체인에 등록
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-
     private final UserRepository userRepository;
-//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-//    private final JwtExceptionFilter jwtExceptionFilter;
-//    private final CustomAccessDeniedHandler customAccessDeniedHandler;
-//    private final CustomAuthenticationEntryPoint customAuthenticationEntryPoint;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     @Autowired
-    public SecurityConfig(UserRepository userRepository
-//                          JwtAuthenticationFilter jwtAuthenticationFilter,
-//                          JwtExceptionFilter jwtExceptionFilter,
-//                          CustomAccessDeniedHandler customAccessDeniedHandler,
-//                          CustomAuthenticationEntryPoint customAuthenticationEntryPoint
-    ){
+    public SecurityConfig(UserRepository userRepository,
+                          JwtAuthenticationFilter jwtAuthenticationFilter){
         this.userRepository = userRepository;
-//        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-//        this.jwtExceptionFilter = jwtExceptionFilter;
-//        this.customAccessDeniedHandler = customAccessDeniedHandler;
-//        this.customAuthenticationEntryPoint = customAuthenticationEntryPoint;
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
     }
 
 
@@ -46,10 +36,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable() // csrf 미적용
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 토큰 기반 인증이므로 세션 사용 하지않음
                 .and()
-//                .httpBasic().authenticationEntryPoint(customAuthenticationEntryPoint)   // 인증 되지 않은 유저가 요청했을때 동작
-//                .and()
-//                .exceptionHandling().accessDeniedHandler(customAccessDeniedHandler) // 액세스 할 수 없는 요청 했을 시 동작
-//                .and()
                 .authorizeRequests()
                 .antMatchers("/api/user/login", "/api/seller/login", "/api/seller/signup", "/api/seller/email", "/api/seller/emailConfirm").permitAll()
                 //.antMatchers("/api/user/product/list/**", "/api/user/product/{\\d+}").permitAll()
@@ -57,8 +43,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/seller/**").hasRole("ADMIN")
                 .anyRequest().permitAll()
                 .and().cors();
-        //http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // controller 시작 전에 jwt 인증을 하기 위한 필터 등록
-        //http.addFilterBefore(jwtExceptionFilter, JwtAuthenticationFilter.class); // jwt 인증 시 발생하는 exception 처리 필터 등록
+        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // controller 시작 전에 jwt 인증을 하기 위한 필터 등록
 
     }
 
