66bdd86f353c6c72acd12543d47deb3fbab717a5
ksh940925 <ksh9409255@naver.com>
2022-10-27 09:32:47 +0900

    fix: feignClient url 수정 및 feignClient 적용

--- src/main/java/com/team6/onandthefarmmemberservice/OnandthefarmMemberserviceApplication.java
+++ src/main/java/com/team6/onandthefarmmemberservice/OnandthefarmMemberserviceApplication.java
@@ -2,8 +2,10 @@ package com.team6.onandthefarmmemberservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class OnandthefarmMemberserviceApplication {
 
 	public static void main(String[] args) {
