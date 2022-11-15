15f1b0440a2d1e4982a1df76d395d89dbf0c5bc5
jikimomo <jikimomo@gmail.com>
2022-11-15 14:33:52 +0900

    feat : feed 공유 url 접근 시 point 지급 로직 추가

---
+++ src/main/java/com/team6/onandthefarmmemberservice/feignclient/vo/UserIdVo.java
@@ -0,0 +1,12 @@
+package com.team6.onandthefarmmemberservice.feignclient.vo;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserIdVo {
+    private Long userId;
+}
